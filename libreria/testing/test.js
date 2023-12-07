0cbc8a9a5e66707aa2e0f7677f0a338c48ecc279
diego aaron <diegoaarondv@gmail.com>
2023-12-06 21:32:24 -0500

    dev testing 5/n

--- libreria/testing/test.js
+++ libreria/testing/test.js
@@ -1,9 +1,11 @@
-describe("test ruta get /", () => {
-  it("Deberia salir algo", async () => {
+import { jest } from "@jest/globals";
+import fetch from "node-fetch";
+
+jest.describe("test ruta get /", () => {
+  jest.it("Deberia salir algo", async () => {
     const response = await fetch("http://localhost:3000/");
-    let body = await response.body;
-    console.log(body);
-    expect(response.status).toBe(200);
-    // expect(response.body).toBe("app up!");
+    // console.log(body);
+    jest.expect(response.status).jest.toBe(200);
+    // expect(response.body.text()).toBe("app up!");
   });
 });
