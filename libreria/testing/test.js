d820baa7803c1a619a933551146e4e3216ff9e0c
diego aaron <diegoaarondv@gmail.com>
2023-12-06 20:41:57 -0500

    dev testing 4/n

--- libreria/testing/test.js
+++ libreria/testing/test.js
@@ -1,18 +1,9 @@
-// import { fetch } from "../node_modules/node-fetch/src/";
-
 describe("test ruta get /", () => {
   it("Deberia salir algo", async () => {
-    const response = await fetch("https://api.github.com/users/github");
+    const response = await fetch("http://localhost:3000/");
+    let body = await response.body;
+    console.log(body);
     expect(response.status).toBe(200);
-    expect(response.body).toBe("app up!");
+    // expect(response.body).toBe("app up!");
   });
 });
-
-// test("add 1", () => {
-//   expect(1).toBe(1);
-// });
-
-// test("add 2", async () => {
-//   let test = await fetch("https://www.google.com.pe");
-//   expect(test).toBe("x");
-// });
