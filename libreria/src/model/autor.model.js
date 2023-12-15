50363102341532e4a757061c3f740d5f8601593a
diego aaron <diegoaarondv@gmail.com>
2023-12-15 17:09:10 -0500

    update model 2/n

--- libreria/src/model/autor.model.js
+++ libreria/src/model/autor.model.js
@@ -3,66 +3,38 @@ import { DateTime, Interval } from "luxon";
 
 const Schema = mongoose.Schema;
 
-const AuthorSchema = new Schema({
-  firstName: { type: String, require: true, maxLength: 100 },
-  lastName: { type: String, require: true, maxLength: 100 },
-  email: { type: String, maxLength: 100 },
-  dateOfBirthday: { type: Date },
-  dateOfDeath: { type: Date },
+const AutorSchema = new Schema({
+  nombres: { type: String, require: true, maxLength: 100 },
+  apellidos: { type: String, require: true, maxLength: 100 },
+  fechaNacimiento: { type: Date },
+  fechaMuerte: { type: Date },
 });
 
-// nombre y apellido
+// campo virtual: nombre completo
 
-AuthorSchema.virtual("fullName").get(function () {
+AutorSchema.virtual("nombreCompleto").get(function () {
   let fullName = "";
 
-  if (this.firstName && this.lastName) {
-    fullName = `${this.firstName} ${this.lastName}`;
+  if (this.nombres && this.apellidos) {
+    fullName = `${this.nombres} ${this.apellidos}`;
   }
 
   return fullName;
 });
 
-// edad
+// campo virtual: edad
 
-AuthorSchema.virtual("lifespan").get(function () {
-  let fechaActual = this.dateOfDeath
-    ? DateTime.fromJSDate(THIS.dateOfDeath)
+AutorSchema.virtual("edad").get(function () {
+  let fechaActual = this.fechaMuerte
+    ? DateTime.fromJSDate(this.fechaMuerte)
     : DateTime.now();
-  let edad = Interval.fromDateTimes(this.dateOfBirthday, fechaActual).length("years");
+  
+  let edad = Interval.fromDateTimes(this.fechaNacimiento, fechaActual).length("years");
   edad = Math.round(edad);
+  
   return edad;
 });
 
-// fecha de nacimiento formateada
+const Autor = mongoose.model("Autor", AutorSchema);
 
-AuthorSchema.virtual("dateOfBirthFormatted").get(function () {
-  let fechaDeNacimiento = this.dateOfBirthday ? this.dateOfBirthday : "";
-  if (fechaDeNacimiento === "") {
-    return "fecha de nacimiento no registrada";
-  }
-  fechaDeNacimiento = DateTime.fromJSDate(this.dateOfBirthday).toLocaleString(
-    Datetime.DATE_FULL
-  );
-  return fechaDeNacimiento;
-});
-
-// fecha de muerte formateada
-
-AuthorSchema.virtual("dateOfDeadFormatted").get(function () {
-  let fechaDeMuerte = this.dateOfDeath ? this.dateOfDeath : "";
-  if (fechaDeMuerte === "") {
-    return "fecha de muerte no registrada";
-  }
-  fechaDeMuerte = DateTime.fromJSDate(this.fechaDeMuerte).toLocaleString(
-    Datetime.DATE_FULL
-  );
-  return fechaDeMuerte;
-});
-
-const Author = mongoose.model("Author", AuthorSchema);
-
-export { Author };
-
-
-// agregar a la imagen del modelo author los 2 campos virtuales
+export { Autor };
