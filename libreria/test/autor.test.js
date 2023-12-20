828fa2d73cfc0e9c9f08546316051c88d32f43fe
diego aaron <diegoaarondv@gmail.com>
2023-12-20 16:28:08 -0500

    update test 1/n

--- libreria/test/autor.test.js
+++ libreria/test/autor.test.js
@@ -1,28 +1,17 @@
 import test from "ava";
-
 import fetch from "node-fetch";
 
-// jest.describe("test ruta get /", () => {
-//   jest.it("Deberia salir algo", async () => {
-//     const response = await fetch("http://localhost:3000/");
-//     // console.log(body);
-//     jest.expect(response.status).jest.toBe(200);
-//     // expect(response.body.text()).toBe("app up!");
-//   });
-// });
+test("cleanup", (t) => {
+  t.pass("saltando un test");
+});
+
 test("foo", (t) => {
-  t.pass();
+  t.pass("saltando un test");
 });
 
-test("get /", async (t) => {
+test("get localhost:3000/", async (t) => {
   const response = await fetch("http://localhost:3000/");
-  t.is(response.status, 200);
-  // t.is(response.body.text(), "app up!");
-});
-
-test.after("cleanup", (t) => {
-  // This runs after all tests
-  console.log("hi!");
+  t.is(response.status, 200, "!completado");
 });
 
 // https://github.com/avajs/ava/blob/5975b602b771e0dc02382d24b65c5561bd5fc7ee/docs/03-assertions.md
