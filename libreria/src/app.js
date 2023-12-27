4e5fbf8953607930304df19bf3adb7161d128994
diego aaron <diegoaarondv@gmail.com>
2023-12-26 21:01:17 -0500

    documentacion 1/n

--- libreria/src/app.js
+++ libreria/src/app.js
@@ -6,6 +6,23 @@ import { openmongoconexion } from "../config/mongo.conexion.js";
 
 openmongoconexion(urlmongoconexion);
 
+// documentacion
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.1",
+    info: {
+      title: "Documentacion API Libreria",
+      version: "1.1.1",
+    },
+  },
+  apis: ["./routes/*.js"],
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+
 // routes
 import routerAutor from "./routes/autor.routes.js";
 import routerLibro from "./routes/libro.routes.js";
@@ -15,6 +32,8 @@ const app = express();
 
 // midleware
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification)); // documentacion
 
 app.use(routerAutor);
 app.use(routerLibro);
