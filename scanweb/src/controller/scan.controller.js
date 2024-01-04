ef58f9620af1d3e9d03ad5960ce30ecdaf4f4118
diego aaron <diegoaarondv@gmail.com>
2024-01-04 00:36:54 -0500

    update scan

--- scanweb/src/controller/scan.controller.js
+++ scanweb/src/controller/scan.controller.js
@@ -1,7 +1,34 @@
 import { Scan } from "../model/scan.model.js";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
+// import puppeteer from "puppeteer";
+
+// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
+puppeteer.use(StealthPlugin());
+
+// Add adblocker plugin to block all ads and trackers (saves bandwidth)
+
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 
 // login en la web
+
+function waitforme(millisec) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("");
+    }, millisec);
+  });
+}
+
+async function printy() {
+  for (let i = 0; i < 2; ++i) {
+    await waitforme(1000);
+    console.log(i);
+  }
+  console.log("Loop execution finished!)");
+}
 
 const loginInWeb = async function (req, res) {
   try {
@@ -23,11 +50,22 @@ const loginInWeb = async function (req, res) {
 
     const logintag =
       ".modulor_field__field-input__1_32_8.modulor_field__with-label__1_32_8.modulor_field__active__1_32_8";
-    await page.waitForSelector(logintag);
 
     const passtag =
       ".modulor_field__field-input__1_32_8.modulor_field__with-label__1_32_8.modulor_field__with-action-icon__1_32_8";
-    
+
+    const buttontag =
+      ".modulor_button__button__1_32_8.modulor_button__primary__1_32_8.modulor_button__regular__1_32_8";
+
+    await page.waitForSelector(logintag);
+
+    await page.type(logintag, "999111222");
+    await printy();
+    await page.type(passtag, "@new");
+    await printy();
+    await page.click(buttontag);
+    await printy();
+
     // Locate the full title with a unique string
     // const textSelector = await page.waitForSelector("text/Customize and automate");
     // const fullTitle = await textSelector?.evaluate((el) => el.textContent);
