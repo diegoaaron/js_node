18ed9579d42d4f5bed8523740a80252319a2003f
diego aaron <diegoaarondv@gmail.com>
2024-01-03 23:03:34 -0500

    update loging

--- scanweb/src/controller/scan.controller.js
+++ scanweb/src/controller/scan.controller.js
@@ -1,13 +1,48 @@
 import { Scan } from "../model/scan.model.js";
+import puppeteer from "puppeteer";
 
 // login en la web
 
 const loginInWeb = async function (req, res) {
   try {
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    // Navigate the page to a URL
+    await page.goto("https://pe.parimatch.com/es/");
+
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    // Wait and click on first result
+    const searchResultSelector =
+      ".modulor_button__button__1_32_8.modulor_button__always_white__1_32_8.modulor_button__low__1_32_8";
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+
+    const logintag =
+      ".modulor_field__field-input__1_32_8.modulor_field__with-label__1_32_8.modulor_field__active__1_32_8";
+    await page.waitForSelector(logintag);
+
+    const passtag =
+      ".modulor_field__field-input__1_32_8.modulor_field__with-label__1_32_8.modulor_field__with-action-icon__1_32_8";
+    
+    // Locate the full title with a unique string
+    // const textSelector = await page.waitForSelector("text/Customize and automate");
+    // const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+
+    // Print the full title
+    // console.log('The title of this blog post is "%s".', fullTitle);
+
+    // await browser.close();
   } catch (error) {
-    res.status(500).send(error);
+    console.log(error);
+    //   res.status(500).send(error);
   }
 };
+
+await loginInWeb();
 
 // leer apuestas de futbol
 
