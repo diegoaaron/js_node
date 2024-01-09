b6f4ad97e280a0a8ce28fa20f63da5df4df2b1b1
diego aaron <diegoaarondv@gmail.com>
2024-01-08 20:45:42 -0500

    update interview-prep 1/n

---
+++ interviewprep/algoritmos/temp.js
@@ -0,0 +1,46 @@
+function permAlone(str) {
+  // Creamos una expresión regular que filtre las cadenas con palabras repetidas
+  let regex = /(.)\1+/;
+
+  // Dividiendo la cadena en un array de caracteres individuales
+  const arr = str.split("");
+  const permutations = [];
+  let tmp;
+
+  // Return 0 if str contains same character.
+  if (str.match(regex) !== null && str.match(regex)[0] === str) return 0;
+
+  // Function to swap variables' content.
+  function swap(index1, index2) {
+    tmp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = tmp;
+  }
+
+  // Generate arrays of permutations using the algorithm.
+  function generate(int) {
+    if (int === 1) {
+      // Make sure to join the characters as we create  the permutation arrays
+      permutations.push(arr.join(""));
+    } else {
+      for (let i = 0; i != int; ++i) {
+        generate(int - 1);
+        swap(int % 2 ? 0 : i, int - 1);
+      }
+    }
+  }
+
+  generate(arr.length);
+
+  // Filter the array of repeated permutations.
+  const filtered = permutations.filter(function (string) {
+    return !string.match(regex);
+  });
+
+  // Return how many have no repetitions.
+  return filtered;
+}
+
+// Test here.
+let xyz = permAlone("aab");
+console.log(xyz);
