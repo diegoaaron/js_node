6fd822d267ee3f49f9f271b416a1b05bbf736605
diego aaron <diegoaarondv@gmail.com>
2024-01-15 22:29:03 -0500

    update interview-prep 16/n

--- interviewprep/algoritmos/temp1.js
+++ interviewprep/algoritmos/temp1.js
@@ -1,39 +1,29 @@
-//intercambiamos valores utilizando desestructuración (ES6)
-function swap(arr, x, y) {
-  [arr[x], arr[y]] = [arr[y], arr[x]];
-}
-
-// Obtenemos el punto de pivote fijo (hay 2 parametros con valores por defecto)
-function pivot(arr, left = 0, right = arr.length - 1) {
-  let shift = left;
-
-  for (let i = left + 1; i <= right; i++) {
-    // Movemos los elementos pequeños al lado izquierdo
-    if (arr[i] < arr[left]) {
-      swap(arr, i, ++shift);
-    }
+//Merger function, which merges 2 sorted array into 1 sorted array
+function merger(arr1, arr2) {
+  let i = 0,
+    j = 0,
+    mergedArr = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] > arr2[j]) mergedArr.push(arr2[j++]);
+    else mergedArr.push(arr1[i++]);
   }
-
-  // Finalmente intercambiamos el último elemento con el de la izquierda
-  swap(arr, left, shift);
-
-  return shift;
-}
-
-// funcion principal de ordenamiento con recursividad (hay 2 parametros con valores por defecto)
-function quickSort(array, left = 0, right = array.length - 1) {
-  if (left < right) {
-    let pivotIndex = pivot(array, left, right);
-
-    //Recusrively calling the function to the left of the pivot and to the right of the pivot
-    quickSort(array, left, pivotIndex - 1);
-    quickSort(array, pivotIndex + 1, right);
+  while (i < arr1.length) {
+    mergedArr.push(arr1[i++]);
   }
-  return array;
+  while (j < arr2.length) {
+    mergedArr.push(arr2[j++]);
+  }
+  return mergedArr;
 }
+function mergeSort(array) {
+  //Array of length 1 is sorted so we return the same array back
+  if (array.length == 1) return array;
 
-let resultado = quickSort([
-  1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92,
-]);
+  //Break down the array to half from middle into left and right
+  let middle = Math.floor(array.length / 2);
+  let left = mergeSort(array.slice(0, middle));
+  let right = mergeSort(array.slice(middle));
 
-console.log(resultado);
+  //Return the merged sorted array
+  return merger(left, right);
+}
