df802ea19f0bf0d9c86403d5ac43daf9f456ccd1
diego aaron <diegoaarondv@gmail.com>
2024-01-17 13:06:45 -0500

    update interview-prep 17/n

--- interviewprep/algoritmos/temp1.js
+++ interviewprep/algoritmos/temp1.js
@@ -1,25 +1,33 @@
 //Merger function, which merges 2 sorted array into 1 sorted array
 function merger(arr1, arr2) {
-  let i = 0,
-    j = 0,
-    mergedArr = [];
+  let i = 0;
+  let j = 0;
+  let mergedArr = [];
+
   while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] > arr2[j]) mergedArr.push(arr2[j++]);
-    else mergedArr.push(arr1[i++]);
+    if (arr1[i] > arr2[j]) {
+      mergedArr.push(arr2[j++]);
+    } else {
+      mergedArr.push(arr1[i++]);
+    }
   }
+
   while (i < arr1.length) {
     mergedArr.push(arr1[i++]);
   }
+
   while (j < arr2.length) {
     mergedArr.push(arr2[j++]);
   }
+
   return mergedArr;
 }
+
 function mergeSort(array) {
-  //Array of length 1 is sorted so we return the same array back
+  //Evaluando si el array solo tiene 1 elemento
   if (array.length == 1) return array;
 
-  //Break down the array to half from middle into left and right
+  //Dividiendo el array en 2 partes
   let middle = Math.floor(array.length / 2);
   let left = mergeSort(array.slice(0, middle));
   let right = mergeSort(array.slice(middle));
@@ -27,3 +35,7 @@ function mergeSort(array) {
   //Return the merged sorted array
   return merger(left, right);
 }
+
+let xyz = mergeSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+
+console.log(xyz);
